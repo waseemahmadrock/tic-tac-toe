c2797557d53d609a6cf8240a9aee3127080711a0
Waseem Akhtar <waseemahmadrock@gmail.com>
2019-07-22 16:22:12 +0500

    Updates

---
+++ precache-manifest.b0156a82523ffb5a33824dfa3b5df0b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84856d11c32592a0fdd03ad1005b692a",
+    "url": "/tic-tac-toe/static/media/github.84856d11.svg"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "3dfe725290cd511a282a",
+    "url": "/tic-tac-toe/static/js/main.3dfe7252.chunk.js"
+  },
+  {
+    "revision": "6e110f560a629ff07463",
+    "url": "/tic-tac-toe/static/js/1.6e110f56.chunk.js"
+  },
+  {
+    "revision": "3dfe725290cd511a282a",
+    "url": "/tic-tac-toe/static/css/main.ddde29b0.chunk.css"
+  },
+  {
+    "revision": "2e471ee60d428c66bf86313806857138",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe-ai/precache-manifest.2a3642de1c8ad54230769eef30127d0c.js"
+  "/tic-tac-toe/precache-manifest.b0156a82523ffb5a33824dfa3b5df0b6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tic-tac-toe-ai/index.html", {
+workbox.routing.registerNavigationRoute("/tic-tac-toe/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
